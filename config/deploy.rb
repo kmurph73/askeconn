c21232248604f124f980acdc56b939f1838db780
Kyle Murphy <kyle@kyle-murphys-macbook-pro.local>
2008-12-19 23:37:37 -0800

    Initial Commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :domain, "ask-economists.com"
 set :application, "askeconn"
 set :repository,  "deploy@72.14.179.64:/home/deploy/askeconn.git"
 set :user, "deploy"
-set :use_sudo, true
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
