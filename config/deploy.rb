0b53bfc6de0631e1e0c8462f22ef3a4bd594c663
Kyle Murphy <kyle@kyle-murphys-macbook-pro.local>
2008-12-19 20:14:42 -0800

    alkdj

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,7 @@ set :domain, "ask-economists.com"
 set :application, "askeconn"
 set :repository,  "deploy@72.14.179.64:/home/deploy/askeconn.git"
 set :user, "deploy"
-set :scm_verbose, true
-default_run_options[:pty] = true
+set :use_sudo, true
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
